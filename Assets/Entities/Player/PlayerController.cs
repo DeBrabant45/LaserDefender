fa03380b30d0c8f6af643d963fe22d9ff74dbe60
Aaron <DeBrabant45@users.noreply.github.com>
2019-05-26 18:34:37 -0400

    Adding wave start timer

--- Assets/Entities/Player/PlayerController.cs
+++ Assets/Entities/Player/PlayerController.cs
@@ -120,7 +120,7 @@ public class PlayerController : MonoBehaviour
         Destroy(gameObject);
         GameObject deathParticle = Instantiate(DeathParticls, transform.position, Quaternion.identity) as GameObject;
         LevelManager man = GameObject.Find("LevelManager").GetComponent<LevelManager>();
-        man.LoadLevel(5);
+        man.LoadLevel(6);
         AudioSource.PlayClipAtPoint(deathSound, transform.position);
     }
 
---
+++ Assets/Scripts/BeginWaveCountDown.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BeginWaveCountDown : MonoBehaviour
+{
+    public float beginWaveTimer = 5.0f;
+    private Text timerText;
+
+    void Start()
+    {
+        timerText = GetComponent<Text>();
+        StartCoroutine("PauseForSeconds");
+        Reset();
+    }
+
+    private IEnumerator PauseForSeconds()
+    {
+        float originalTimeScale = Time.timeScale; // store original time scale in case it was not 1
+        Time.timeScale = 0; // pause
+        while (beginWaveTimer >= 0f)
+        {
+            yield return null; // don't use WaitForSeconds() if Time.timeScale is 0!
+            beginWaveTimer -= Time.unscaledDeltaTime; // returns deltaTime without being multiplied by Time.timeScale
+            timerText.text = beginWaveTimer.ToString("f0");
+        }
+        timerText.gameObject.SetActive(false);
+        Time.timeScale = originalTimeScale; // restore time scale from before pause
+    }
+
+    public void Reset()
+    {
+        beginWaveTimer = 5f;
+    }
+}
